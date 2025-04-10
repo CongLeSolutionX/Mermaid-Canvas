6f493a7ce899f537b023edb1be085cced8b2f7b1
Cong Le <cle155@csu.fullerton.edu>
2025-04-09 23:36:44 -0700

    1st try - lovely work

---
+++ Mermaid_Canvas_SourceCode/Mermaid_Canvas/Mermaid_CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mermaid_CanvasApp.swift
+//  Mermaid_Canvas
+//
+//  Created by Cong Le on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Mermaid_CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
